08523ec881497953047b5fbf317a5be5b78f13cb
Luke Greenway <greenwaylj18@gcc.edu>
2022-05-03 23:57:05 -0400

    Removed graveyard code

--- Code/grove_lang.py
+++ Code/grove_lang.py
@@ -127,22 +127,3 @@ class Call(Expr):
             raise GroveError(e)
 
  
-# some testing code
-# if __name__ == "__main__":
-#     assert(Num(3).eval() == 3)
-#     assert(Addition(Num(3), Num(10)).eval() == 13)
-#     assert(Subtraction(Num(3), Num(10)).eval() == -7)
-    
-#     caught_error = False
-#     try:
-#         print(Name("nope").eval())
-#     except ValueError:
-#         caught_error = True
-#     assert(caught_error)
-    
-#     assert(Stmt(Name("foo"), Num(10)).eval() is None)
-#     assert(Name("foo").eval() == 10)
-    
-#     # Try something more complicated
-#     assert(Stmt(Name("foo"), Addition(Num(200), Subtraction(Num(4), Num(12)))).eval() is None)
-#     assert(Name("foo").eval() == 192)
