04ca80ce382d16af237fa33801bc54f3c3b42e5e
Tirzah <lloydta18@gcc.edu>
2022-05-03 22:37:32 -0400

    Answered the written question

--- Code/grove.py
+++ Code/grove.py
@@ -1,3 +1,10 @@
+# Written Question:
+# Is your Grove interpreter using a static or dynamic type system? Briefly explain what aspects of
+# the interpreter make it so.
+# Answer:
+# Our Grove interpreter is using a dynamic type system. Because our language isn't compiled, the type cannot be known at compile time.
+# Therefore, the type isn't known until runtime, and the user can use the same name to refer to different types at different points in the program.
+
 from grove_lang import *
 from grove_parse import *
 
